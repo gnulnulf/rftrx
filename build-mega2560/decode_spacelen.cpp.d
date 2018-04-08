97a544be1b07da355c3d1f84df34da08c80ff9a3
Arco van Geest <arco@appeltaart.mine.nu>
2018-04-08 11:23:28 +0200

    shell

---
+++ build-mega2560/decode_spacelen.cpp.d
@@ -0,0 +1,13 @@
+build-mega2560/decode_spacelen.cpp.o: decode_spacelen.cpp rftrx.h \
+ /usr/share/arduino/hardware/arduino//cores/arduino/Arduino.h \
+ /usr/share/arduino/hardware/arduino//cores/arduino/binary.h \
+ /usr/share/arduino/hardware/arduino//cores/arduino/WCharacter.h \
+ /usr/share/arduino/hardware/arduino//cores/arduino/WString.h \
+ /usr/share/arduino/hardware/arduino//cores/arduino/HardwareSerial.h \
+ /usr/share/arduino/hardware/arduino//cores/arduino/Stream.h \
+ /usr/share/arduino/hardware/arduino//cores/arduino/Print.h \
+ /usr/share/arduino/hardware/arduino//cores/arduino/Printable.h \
+ /usr/share/arduino/hardware/arduino//cores/arduino/new.h \
+ /usr/share/arduino/hardware/arduino//variants/mega/pins_arduino.h \
+ radio_hardware.h commands.h RFreceive.h RFtransmit.h decode_spacelen.h \
+ frame_decode.h frame_encode.h tools.h SerialCommands.h
